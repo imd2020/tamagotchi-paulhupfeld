50c1dd540f7dc7357f628eafa0df61c484443a1f
paulhupfeld <paul.hupfeld@stud.h-da.de>
2021-06-22 23:12:22 +0200

    transition class created

---
+++ Transition.js
@@ -0,0 +1,43 @@
+class Transition {
+  constructor() {
+    this.transparency = 0;
+    this.change = "gettingHigher";
+  }
+
+  controllTransparency() {
+    if (this.transparency >= 255) {
+      this.change = "gettingLower";
+    }
+
+    if (this.change === "gettingHigher") {
+      this.transparency += 20;
+    } else if (this.change === "gettingLower") {
+      this.transparency -= 20;
+    }
+  }
+
+  stopTransition() {}
+
+  display() {
+    clear();
+    fill(180, 255, 180, this.transparency);
+    rect(0, 0, 600, 600);
+
+    fill(255, 255, 255, this.transparency);
+
+    textAlign(LEFT, LEFT);
+
+    textSize(50);
+
+    text("Sommer 2020", 250, 280, 100, 300);
+    // text("Sommer ) + mainscreen.year, 10, 20);
+
+    this.controllTransparency();
+  }
+}
+
+let transition = new Transition();
+
+function draw() {
+  transition.display();
+}
